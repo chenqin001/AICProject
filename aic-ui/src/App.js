8260b46e7570a66638a65e19081a83876d945bca
Qin <zoecq001@gmail.com>
2022-04-11 18:31:43 -0400

    add CSS

--- aic-ui/src/App.js
+++ aic-ui/src/App.js
@@ -10,33 +10,56 @@ import {
   Navigate
 } from "react-router-dom";
 
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginLeft: theme.spacing(10),
+    display: "flex",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(20),
+    "&:hover": {
+      color: "yellow",
+      borderBottom: "1px solid white",
+    },
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <Router>
       <div className="App">
-
-        <nav>
-          <ul>
-            <li>
-              <Link to="/upload">Upload</Link>
-            </li>
-            <li>
-              <Link to="/download">Download</Link>
-            </li>
-            <li>
-              <Link to="/settings">Settings</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
+      <AppBar position="static">
+      <Toolbar>
+      <CssBaseline />
+      <Typography variant="h4">
+          AIC Final Project 
+        </Typography>
+        <div className={classes.navlinks}>
+            <Link to="/upload" className={classes.link}>Upload</Link>
+            <Link to="/download" className={classes.link}>Download</Link>
+            <Link to="/settings" className={classes.link}>Settings</Link>
+        </div>
+        </Toolbar>
+        </AppBar>
+      </div>
+      <Routes>
           <Route path="/" element={<Navigate to="/Upload" />}/>
           <Route exact path='/upload' element={<Upload/>} />
           <Route path='/download' element={<Download/>} />
           <Route path='/settings' element={<Settings/>} />
         </Routes>
-
-      </div>
     </Router>
   );
 }
